a879e0ddde515a4f3471998eb1c3159dce9b9263
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2023-12-24 20:41:16 +0300

    feat: solve problem with file names

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+    let result = [];
+    const fileNames = new Map();
+    for (let i = 0; i < names.length; i++) {
+        if (!fileNames.has(names[i])) {
+            fileNames.set(names[i], 1);
+            result[i] = names[i];
+        } else {
+            let currentValue = fileNames.get(names[i]);
+            let nextValue = ++currentValue;
+            fileNames.set(names[i], nextValue);
+            let nextFileName = names[i] + `(${nextValue - 1})`;
+            fileNames.set(nextFileName, 1);
+            result[i] = nextFileName;
+        }
+    }
+    return result;
 }
 
 module.exports = {
-  renameFiles
+    renameFiles
 };
